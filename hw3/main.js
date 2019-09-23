db2c73195aa5c2cf95b3cb4250660933a66b90ba
DoperJ <894661167@qq.com>
2019-09-23 17:11:22 -0500

    Add Difficulty Level

--- hw3/main.js
+++ hw3/main.js
@@ -1,55 +1,107 @@
 function Manager() {
     this.weedInterval = undefined;
-    this.canvas = $("#canvas")[0];
+    this.eventTarget = $("#canvas");
+    this.canvas = this.eventTarget[0];
     this.ctx = canvas.getContext("2d");
     this.h = new Horizon(canvas, conf.horizonHeght);
-    this.runner = new Runner(canvas, 10, conf.horizonHeght, 64, 52);
+    this.runner = undefined;
     this.isLost = false;
     this.score = 0;
-    this.runnerInterval = 0
+    this.runnerInterval = 0;
+    this.seaweeds = [];
+    this.seaweeds.push([35, 70, "seaweed1.png"]);
+    this.seaweeds.push([40, 80, "seaweed2.png"]);
+    this.seaweeds.push([70, 80, "seaweed3.png"]);
+    this.level = 1;
+    //console.log(this.seaweeds);
+    let that = this;
+    this.eventTarget.click(function (event) {
+        if (!that.runner.isJumping) {
+            //console.log("up" + that.isLost);
+            if (!that.isLost) {
+                that.runner.jump();
+            }
+        }
+        if (that.isLost) {
+            let x = event.offsetX;
+            let y = event.offsetY;
+            let l = conf.failedImgX, r = conf.failedImgX + conf.failedImgWidth;
+            let t = conf.failedImgY, b = conf.failedImgY + conf.failedImgHeight;
+            if (x >= l && x <= r && y >= t && y <= b) {
+                that.start();
+            }
+            //console.log(x + ", " + y);
+        }
+    });
 }
 
 Manager.prototype = {
+    init: function() {
+        this.isLost = false;
+        this.level = 1;
+        this.score = 0;
+        this.runner = new Runner(this.canvas, 10, conf.horizonHeght, 64, 52);
+    },
     start: function () {
+        this.init();
+        this.ctx.clearRect(0, 0, conf.width, conf.height);
         this.h.draw();
         this.runner.render();
-        this.weedInterval = setInterval(()=>{
-            let seaweed = new SeaWeed(this.canvas, conf.width, conf.horizonHeght, 35, 70, this.runner);
-            seaweed.render();
-        }, 3000);
-        this.ctx.clearRect(0, 0, conf.width, 50);
-        this.ctx.font = '20px sans-serif';
-        //seaweed.render();
         this.drawHighestScore();
-        let that = this;
-        document.onkeydown = function (event) {
-            if (event.key === "ArrowUp" && !that.runner.isJumping) {
-                console.log("up" + that.isLost);
-                if (!that.isLost) {
-                    that.runner.jump();
-                }
-            }
-        };
+        this.drawCurrentScore();
+        this.drawLevel();
+        this.weedInterval = setInterval(() => {
+           let level = parseInt(manager.score / 200) + 1;
+            level = Math.min(level, this.seaweeds.length);
+            this.level = level;
+            let next = parseInt(Math.random() * level);
+            //console.log(level + ", " + next);
+            this.drawLevel();
+            let nextSeaweedConfig = manager.seaweeds[next];
+            let seaweed = new SeaWeed(this.canvas, conf.width, conf.horizonHeght,
+                nextSeaweedConfig[0], nextSeaweedConfig[1], this.runner, nextSeaweedConfig[2]);
+            seaweed.render();
+        }, 3000 - this.level * 500);
+        //seaweed.render();
     },
     drawHighestScore: function() {
+        this.ctx.clearRect(0, 0, conf.width, 40);
+        this.ctx.font = '20px sans-serif';
         let highestScore = $.cookie('highest_score');
         highestScore = highestScore == null ? 0 : highestScore;
-        this.ctx.fillText('Highest Score is: ' + highestScore, 250, 50, 300);
+        this.ctx.fillText('Highest Score: ' + highestScore, 250, 50, 300);
+    },
+    drawCurrentScore: function() {
+        this.ctx.clearRect(0, 50, conf.width, 40);
+        this.ctx.font = '20px sans-serif';
+        this.ctx.fillText('Current Score: ' + manager.score, 250, 80, 300);
+    },
+    drawLevel: function() {
+        this.ctx.clearRect(0, 80, conf.width, 40);
+        this.ctx.font = '20px sans-serif';
+        this.ctx.fillText('Difficulty Level: ' + this.level, 250, 110, 300);
     },
     stop: function () {
         clearInterval(this.weedInterval);
     },
     fail: function () {
         this.stop();
-        console.log("gg");
-        setInterval(()=>{
-            this.ctx.clearRect(0, 0, conf.width, conf.height);
-            this.ctx.font = '20px sans-serif';
-            this.ctx.fillText('You Lost! Your Score is: ' + manager.score, conf.width / 2 - 150, conf.height / 2, 300);
-            let highestScore = $.cookie('highest_score');
-            highestScore = highestScore == null ? manager.score : Math.max(highestScore, manager.score);
-            $.cookie('highest_score', highestScore);
-        }, 1);
+        //console.log("gg");
+        this.ctx.clearRect(0, 0, conf.width, conf.height);
+        this.ctx.font = '20px sans-serif';
+        this.ctx.fillText('You lost! Your score is ' + manager.score, conf.width / 2 - 100, conf.height / 2 - 80, 300);
+        this.ctx.fillText('Click the image to restart...', conf.width / 2 - 100, conf.height / 2 - 40, 300);
+        let highestScore = $.cookie('highest_score');
+        highestScore = highestScore == null ? manager.score : Math.max(highestScore, manager.score);
+        $.cookie('highest_score', highestScore);
+        let failedImg = document.createElement("img");
+        failedImg.src = conf.failedImgSrc;
+        let that = this;
+        failedImg.onload = function() {
+            //console.log(that.img);
+            that.ctx.drawImage(failedImg, conf.failedImgX, conf.failedImgY, conf.failedImgWidth, conf.failedImgHeight);
+        };
+        manager.isLost = true;
         //this.start();
     }
 };
@@ -88,13 +140,10 @@ function Runner(canvas, x, y, width, height) {
 
 Runner.prototype = {
     render: function () {
-        //img.src = "docker.png";
-        //this.img.src = "docker-bubble.jp";
-        //this.img.src = "2.gif";
         this.img.src = "cute-whale.png";
         let that = this;
         this.img.onload = function() {
-            console.log(that.img);
+            //console.log(that.img);
             that.ctx.drawImage(that.img, that.x, that.y, that.width, that.height);
         }
     },
@@ -125,7 +174,7 @@ Runner.prototype = {
     }
 };
 
-function SeaWeed(canvas, x, y, width, height, runner) {
+function SeaWeed(canvas, x, y, width, height, runner, url) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.x = x;
@@ -134,11 +183,12 @@ function SeaWeed(canvas, x, y, width, height, runner) {
     this.height = height;
     this.img = document.createElement("img");
     this.runner = runner;
+    this.url = url;
 }
 
 SeaWeed.prototype = {
     render: function () {
-        this.img.src = "seaweed1.png";
+        this.img.src = this.url;
         let that = this;
         this.img.onload = function () {
             let interval = setInterval(function () {
@@ -148,7 +198,6 @@ SeaWeed.prototype = {
                     that.dir = 0;
                 }*/
                 if (that.kill()) {
-                    manager.isLost = true;
                     //that.runner.render();
                     clearInterval(interval);
                     clearInterval(manager.runnerInterval);
@@ -159,6 +208,7 @@ SeaWeed.prototype = {
                 if (that.x + that.width <= 0) {
                     clearInterval(interval);
                     manager.score += 100;
+                    manager.drawCurrentScore();
                     //console.log(manager.score);
                 }
             }, 1);
@@ -180,6 +230,11 @@ function Config() {
     this.jumpHeight = 150;
     this.speed = 4;
     this.horizonHeght = 50;
+    this.failedImgX = 100;
+    this.failedImgY = 200;
+    this.failedImgWidth = 160;
+    this.failedImgHeight = 120;
+    this.failedImgSrc = "nyan-whale.gif";
 }
 
 $(function () {
